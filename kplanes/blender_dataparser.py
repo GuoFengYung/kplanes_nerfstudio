9780d603e4c4886da7b24b573b4aa13cc5c14f88
kuo <cses955203@gmail.com>
2023-10-19 12:27:59 +0800

    test

---
+++ kplanes/blender_dataparser.py
@@ -0,0 +1,190 @@
+# Copyright 2022 the Regents of the University of California, Nerfstudio Team and contributors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Data parser for blender dataset"""
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+from pathlib import Path
+from typing import Type
+
+import imageio
+import numpy as np
+import torch
+
+from nerfstudio.cameras.cameras import Cameras, CameraType, CAMERA_MODEL_TO_TYPE
+from nerfstudio.data.dataparsers.base_dataparser import (
+    DataParser,
+    DataParserConfig,
+    DataparserOutputs,
+)
+from nerfstudio.data.scene_box import SceneBox
+from nerfstudio.utils.colors import get_color
+from nerfstudio.utils.io import load_from_json
+from nerfstudio.cameras import camera_utils
+from nerfstudio.utils.rich_utils import CONSOLE
+
+
+@dataclass
+class BlenderDataParserConfig(DataParserConfig):
+    """Blender dataset parser config"""
+
+    _target: Type = field(default_factory=lambda: Blender)
+    """target class to instantiate"""
+    data: Path = Path("data/blender/lego")
+    """Directory specifying location of data."""
+    scale_factor: float = 1.0
+    """How much to scale the camera origins by."""
+    alpha_color: str = "white"
+    """alpha color of background"""
+
+
+@dataclass
+class Blender(DataParser):
+    """Blender Dataset
+    Some of this code comes from https://github.com/yenchenlin/nerf-pytorch/blob/master/load_blender.py#L37.
+    """
+
+    config: BlenderDataParserConfig
+
+    def __init__(self, config: BlenderDataParserConfig):
+        super().__init__(config=config)
+        self.data: Path = config.data
+        self.scale_factor: float = config.scale_factor
+        self.alpha_color = config.alpha_color
+
+    def _generate_dataparser_outputs(self, split="train"):
+        if self.alpha_color is not None:
+            alpha_color_tensor = get_color(self.alpha_color)
+        else:
+            alpha_color_tensor = None
+
+        meta = load_from_json(self.data / f"transforms_{split}.json")
+        image_filenames = []
+        poses = []
+
+        fx_fixed = "fl_x" in meta
+        fy_fixed = "fl_y" in meta
+        cx_fixed = "cx" in meta
+        cy_fixed = "cy" in meta
+        height_fixed = "h" in meta
+        width_fixed = "w" in meta
+        distort_fixed = False
+        for distort_key in ["k1", "k2", "k3", "p1", "p2"]:
+            if distort_key in meta:
+                distort_fixed = True
+                break
+        fx = []
+        fy = []
+        cx = []
+        cy = []
+        height = []
+        width = []
+        distort = []
+
+        for frame in meta["frames"]:
+            fname = self.data / Path(frame["file_path"].replace("./", ""))
+
+            if not fx_fixed:
+                assert "fl_x" in frame, "fx not specified in frame"
+                fx.append(float(frame["fl_x"]))
+            if not fy_fixed:
+                assert "fl_y" in frame, "fy not specified in frame"
+                fy.append(float(frame["fl_y"]))
+            if not cx_fixed:
+                assert "cx" in frame, "cx not specified in frame"
+                cx.append(float(frame["cx"]))
+            if not cy_fixed:
+                assert "cy" in frame, "cy not specified in frame"
+                cy.append(float(frame["cy"]))
+            if not height_fixed:
+                assert "h" in frame, "height not specified in frame"
+                height.append(int(frame["h"]))
+            if not width_fixed:
+                assert "w" in frame, "width not specified in frame"
+                width.append(int(frame["w"]))
+            if not distort_fixed:
+                distort.append(
+                    camera_utils.get_distortion_params(
+                        k1=float(frame["k1"]) if "k1" in frame else 0.0,
+                        k2=float(frame["k2"]) if "k2" in frame else 0.0,
+                        k3=float(frame["k3"]) if "k3" in frame else 0.0,
+                        k4=float(frame["k4"]) if "k4" in frame else 0.0,
+                        p1=float(frame["p1"]) if "p1" in frame else 0.0,
+                        p2=float(frame["p2"]) if "p2" in frame else 0.0,
+                    )
+                )
+
+            image_filenames.append(fname)
+            poses.append(np.array(frame["transform_matrix"]))
+        poses = np.array(poses).astype(np.float32)
+
+        # img_0 = imageio.v2.imread(image_filenames[0])
+        # image_height, image_width = img_0.shape[:2]
+        # camera_angle_x = float(meta["camera_angle_x"])
+        # focal_length = 0.5 * image_width / np.tan(0.5 * camera_angle_x)
+
+        # cx = image_width / 2.0
+        # cy = image_height / 2.0
+        camera_to_world = torch.from_numpy(poses[:, :3])  # camera to world transform
+
+        # in x,y,z order
+        camera_to_world[..., 3] *= self.scale_factor
+        scene_box = SceneBox(aabb=torch.tensor([[-1.5, -1.5, -1.5], [1.5, 1.5, 1.5]], dtype=torch.float32))
+
+        if "camera_model" in meta:
+            camera_type = CAMERA_MODEL_TO_TYPE[meta["camera_model"]]
+        else:
+            camera_type = CameraType.PERSPECTIVE
+
+        fx = float(meta["fl_x"]) if fx_fixed else torch.tensor(fx, dtype=torch.float32)
+        fy = float(meta["fl_y"]) if fy_fixed else torch.tensor(fy, dtype=torch.float32)
+        cx = float(meta["cx"]) if cx_fixed else torch.tensor(cx, dtype=torch.float32)
+        cy = float(meta["cy"]) if cy_fixed else torch.tensor(cy, dtype=torch.float32)
+        height = int(meta["h"]) if height_fixed else torch.tensor(height, dtype=torch.int32)
+        width = int(meta["w"]) if width_fixed else torch.tensor(width, dtype=torch.int32)
+        print(fx, fy)
+        if distort_fixed:
+            distortion_params = camera_utils.get_distortion_params(
+                k1=float(meta["k1"]) if "k1" in meta else 0.0,
+                k2=float(meta["k2"]) if "k2" in meta else 0.0,
+                k3=float(meta["k3"]) if "k3" in meta else 0.0,
+                k4=float(meta["k4"]) if "k4" in meta else 0.0,
+                p1=float(meta["p1"]) if "p1" in meta else 0.0,
+                p2=float(meta["p2"]) if "p2" in meta else 0.0,
+            )
+        else:
+            distortion_params = torch.stack(distort, dim=0)
+
+        cameras = Cameras(
+            camera_to_worlds=camera_to_world,
+            fx=fx,
+            fy=fy,
+            cx=cx,
+            cy=cy,
+            distortion_params=distortion_params,
+            height=height,
+            width=width,
+            camera_type=camera_type,
+        )
+
+        dataparser_outputs = DataparserOutputs(
+            image_filenames=image_filenames,
+            cameras=cameras,
+            alpha_color=alpha_color_tensor,
+            scene_box=scene_box,
+            dataparser_scale=self.scale_factor,
+        )
+
+        return dataparser_outputs
